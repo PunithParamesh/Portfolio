dc7e7b70c0e2206b33224a6c5d9a7b3f7e36db02
PunithParamesh <1ms20ec070@gmail.com>
2025-04-20 02:46:05 +0530

    Travel explorer project added

--- src/projects.data.js
+++ src/projects.data.js
@@ -3,9 +3,16 @@ export  const projects=[
     {
         title:"Task Manager Application",
         skills:["React.js", "Express.js", "Mongodb"],
-        desc:" Developed a full-stack Task Manager web application enabling users to perform CRUD operations on personal tasks with real-time updates. Implemented user authentication and authorization using JWT, ensuring secure access with register and login functionality. Built using React.js (frontend), Express.js Node.js (backend), and MongoDB Atlas for database management with RESTful APIs.", 
+        desc:" A full-stack Task Manager web application enabling users to perform CRUD operations on personal tasks with real-time updates. Implemented user authentication and authorization using JWT, ensuring secure access with register and login functionality. Built using React.js (frontend), Express.js Node.js (backend), and MongoDB Atlas for database management with RESTful APIs.", 
         githubLink:"https://github.com/PunithParamesh/taskmanager",
         demo:"https://taskmanagerfrontend-delta.vercel.app/login"
+    },
+    {
+        title:" Travel Destination Explorer",
+        skills:["React", "React Router", "Context API"],
+        desc:"A visually engaging frontend-only travel application that allows users to explore various Indian travel destinations categorized by type (Beach, Mountain, Historical, Spiritual, etc. ). Users can mark/unmark places to their wishlist, which is maintained across the app. Used React Router DOM for multi-page feel in a single-page application.", 
+        githubLink:"https://github.com/PunithParamesh/travelDestinationExplorer",
+        demo:"https://travel-destination-explorer-rho.vercel.app/"
     },
     {
         title:"News updates application",
@@ -17,14 +24,14 @@ export  const projects=[
     {
         title:"Inventory Management",
         skills:["Node.js", "Express.js", "EJS"],
-        desc:"Created a secure inventory management application tailored for e-commerce sellers, featuring user authentication withregistration/login. Allows sellers to manage product listings by adding, updating, and deleting items.Architected with Express.js MVC principles and EJS templating to enhance code organization and maintainability.",
+        desc:"A secure inventory management application tailored for e-commerce sellers, featuring user authentication withregistration/login. Allows sellers to manage product listings by adding, updating, and deleting items.Architected with Express.js MVC principles and EJS templating to enhance code organization and maintainability.",
         githubLink:"https://github.com/PunithParamesh/InventoryAPP",
         demo:"https://inventory-app-two-beta.vercel.app/register"
     },
     {
         title:"Ecommerce API’s",
         skills:["Node.js, Express.js, MongoDB, JWT"],
-        desc:"Developed a comprehensive backend API supporting essential e-commerce features: product listing, addition, filtering,rating, and cart management.Implemented JWT-based user authentication for secure access to signup and login functionalities.Built with Express.js and MongoDB, optimizing data handling and backend performance for scalability.",
+        desc:"A comprehensive backend API supporting essential e-commerce features: product listing, addition, filtering,rating, and cart management.Implemented JWT-based user authentication for secure access to signup and login functionalities.Built with Express.js and MongoDB, optimizing data handling and backend performance for scalability.",
         githubLink:"https://github.com/PunithParamesh/EcommerceApi",
         demo:null
     }
